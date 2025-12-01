338f0cd06e9de4c8fffd7eb676f97d70271c4391
Thang612 <dangtrungthang6122003@gmail.com>
2025-12-02 00:49:35 +0700

    add .env file

--- src/Configs/firebase.ts
+++ src/Configs/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDM99q3pO2U7_Got69rzWeQ3O4Jx6var9Q",
-  authDomain: "react-ts-movie.firebaseapp.com",
-  projectId: "react-ts-movie",
-  storageBucket: "react-ts-movie.firebasestorage.app",
-  messagingSenderId: "278720678936",
-  appId: "1:278720678936:web:bef43aa0c4bf86e96c476d",
-  measurementId: "G-13HMHBL9YC"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
